d408027d10fa66fe0077825ceccdc3c2b7c818c6
Paul Cook <1893351+Simcon@users.noreply.github.com>
2021-12-03 16:48:50 +0000

    Initial commit

---
+++ OrleansTestClusterExperiment/CardGrain.cs
@@ -0,0 +1,35 @@
+﻿using Orleans;
+using Orleans.Runtime;
+
+namespace OrleansTestClusterExperiment
+{
+    public interface ICardGrain : IGrainWithGuidKey
+    {
+        public Task SetTitle(string title);
+        public Task<string> GetTitle();
+    }
+
+    public class CardGrain : Grain, ICardGrain
+    {
+        private readonly IPersistentState<CardState> _card;
+
+        public CardGrain([PersistentState("card")] IPersistentState<CardState> card)
+        {
+            _card = card;
+        }
+
+        public async Task SetTitle(string title)
+        {
+            _card.State.Title = title;
+            await _card.WriteStateAsync();
+        }
+
+        public Task<string> GetTitle() => Task.FromResult(_card.State.Title);
+    }
+
+    [Serializable]
+    public class CardState
+    {
+        public string Title { get; set; } = string.Empty;
+    }
+}
---
+++ OrleansTestClusterExperiment/Tests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using Orleans;
+using Orleans.Hosting;
+using Orleans.TestingHost;
+
+namespace OrleansTestClusterExperiment
+{
+    public class Tests
+    {
+        private TestCluster _cluster;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            var builder = new TestClusterBuilder();
+            _cluster = builder
+                .AddSiloBuilderConfigurator<TestClusterSiloBuilderConfigurator>()
+                .Build();
+            _cluster.Deploy();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _cluster.StopAllSilos();
+        }
+
+        [Test]
+        public async Task Test()
+        {
+            var id = Guid.NewGuid();
+            var expected = "my title";
+
+            await _cluster.Client.GetGrain<ICardGrain>(id).SetTitle(expected);
+            // GetTitle returns null when it should return "my title"
+            var actual = await _cluster.Client.GetGrain<ICardGrain>(id).GetTitle();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class TestClusterSiloBuilderConfigurator : ISiloConfigurator
+    {
+        public void Configure(ISiloBuilder siloBuilder)
+        {
+            siloBuilder
+                .UseLocalhostClustering()
+                .AddMemoryGrainStorageAsDefault();
+        }
+    }
+}
